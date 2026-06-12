3c43928bc96dc6b22f88ed44a1dc8fde8bff6118
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-12 21:33:14 +0700

    Update AIMenu.cpp

--- src/AIMenu.cpp
+++ src/AIMenu.cpp
@@ -1,15 +1,81 @@
-#pragma once
 #include <Geode/Geode.hpp>
+#include "AIMenu.hpp"
 
 using namespace geode::prelude;
 
-class AIMenu : public CCLayer {
-protected:
-    bool init() override;
+AIMenu* AIMenu::create() {
+    auto ret = new AIMenu();
 
-public:
-    static AIMenu* create();
+    if (ret && ret->init()) {
+        ret->autorelease();
+        return ret;
+    }
 
-    void onGenerate(CCObject* sender);
-    void onClose(CCObject* sender);
-};
+    CC_SAFE_DELETE(ret);
+    return nullptr;
+}
+
+bool AIMenu::init() {
+    if (!CCLayer::init())
+        return false;
+
+    auto winSize =
+        CCDirector::sharedDirector()->getWinSize();
+
+    // Background tối
+    auto bg = CCLayerColor::create(
+        ccc4(0, 0, 0, 180)
+    );
+    this->addChild(bg);
+
+    // Tiêu đề
+    auto title =
+        CCLabelBMFont::create(
+            "AI Level Generator PRO",
+            "bigFont.fnt"
+        );
+
+    title->setPosition(
+        winSize.width / 2,
+        winSize.height / 2 + 80
+    );
+
+    this->addChild(title);
+
+    // Generate Button
+    auto generateLabel =
+        CCLabelBMFont::create(
+            "Generate",
+            "goldFont.fnt"
+        );
+
+    auto generateBtn =
+        CCMenuItemLabel::create(
+            generateLabel,
+            this,
+            menu_selector(AIMenu::onGenerate)
+        );
+
+    // Close Button
+    auto closeLabel =
+        CCLabelBMFont::create(
+            "Close",
+            "goldFont.fnt"
+        );
+
+    auto closeBtn =
+        CCMenuItemLabel::create(
+            closeLabel,
+            this,
+            menu_selector(AIMenu::onClose)
+   void AIMenu::onGenerate(CCObject* sender) {
+    FLAlertLayer::create(
+        "AI Generator",
+        "Generate button clicked!",
+        "OK"
+    )->show();
+}
+
+void AIMenu::onClose(CCObject* sender) {
+    CCDirector::sharedDirector()->popScene();
+}
