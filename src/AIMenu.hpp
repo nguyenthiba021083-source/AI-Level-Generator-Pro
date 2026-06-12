f3bc0696b64bac9311182948c241e1100679071a
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-12 21:43:37 +0700

    Update AIMenu.hpp

--- src/AIMenu.hpp
+++ src/AIMenu.hpp
@@ -5,11 +5,10 @@
 using namespace geode::prelude;
 
 class AIMenu : public CCLayer {
-protected:
-    bool init() override;
-
 public:
     static AIMenu* create();
+
+    bool init() override;
 
     void onGenerate(CCObject* sender);
     void onClose(CCObject* sender);
