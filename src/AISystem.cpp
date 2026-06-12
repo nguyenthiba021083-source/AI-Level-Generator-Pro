b0aaf82999c17f36ebc8699beb65caf298228cf2
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-12 21:51:06 +0700

    Update AISystem.cpp

--- src/AISystem.cpp
+++ src/AISystem.cpp
@@ -1,8 +1,28 @@
 #include "AISystem.hpp"
+
 #include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
 
 void AISystem::generate(std::string prompt) {
-    log::info("AI Prompt: {}", prompt);
+
+    if (prompt == "spike") {
+        log::info("Generate 1 spike");
+    }
+
+    else if (prompt == "10 spikes") {
+        log::info("Generate 10 spikes");
+    }
+
+    else if (prompt == "cube") {
+        log::info("Generate cube");
+    }
+
+    else if (prompt == "portal") {
+        log::info("Generate portal");
+    }
+
+    else {
+        log::info("Unknown command");
+    }
 }
