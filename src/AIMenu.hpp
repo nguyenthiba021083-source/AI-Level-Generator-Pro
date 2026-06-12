ace9258e423dba8b216203389536e287212ea54c
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-12 21:24:23 +0700

    Create AIMenu.hpp

---
+++ src/AIMenu.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+class AIMenu : public CCLayer {
+protected:
+    bool init() override;
+
+public:
+    static AIMenu* create();
+};
